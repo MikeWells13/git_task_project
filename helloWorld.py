3919df8129962cca05c2fc5ba3bdc932d399bd56
Mike <mikewells1301@gmail.com>
2024-03-24 22:18:51 +0000

    updated helloWorld.py to include question with user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,12 @@
 # Ask for user's name using input, save to name variable, then print name
 # Print "Git is awesome!" string
 print("Git is awesome!")
+response = input("Do you agree? ").lower()
+print(response)
+
+if response == "no":
+    print("Fair enough.")
+elif response == "yes":
+    print("Great stuff!")
+else:
+    print("What you talkin' about, Willis?")
